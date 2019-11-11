4554a0230ef982dd5b8d2c339ee70404b4c0619e
Pu Shi <mr.pu.shi@gmail.com>
2019-11-11 22:43:58 +0800

    Updates.

--- assets/battlefield/main.850fc.js
+++ assets/battlefield/main.850fc.js
@@ -129,7 +129,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.0cb8e.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -155,7 +155,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.850fc.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -179,7 +179,7 @@ if (false) {
     window.boot();
 }
 else if (window.jsb) {
-    require('src/settings.js');
+    require('src/settings.00e21.js');
     require('src/cocos2d-jsb.js');
     require('jsb-adapter/engine/index.js');
     window.boot();
